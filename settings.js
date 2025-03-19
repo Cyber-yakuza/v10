6e5c7542c78590482a52218768f09fec1a891825
Cyber yakuza <surathaloka218@gmail.com>
2025-03-19 21:29:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Wu4GgSqa#OroAAXbycraEABwI6bPKb1EAQNrdE27HexQdDIJJoHw' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Czh1lYTS#ACLJrSN_M2y660L6258b-Bqw9_tOZJtpYsqXKPt-cmE' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"private" : process.env.MODE,
