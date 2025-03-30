8273f8970b75f586d31af9b820051ce5f8b9bc4a
Cyber yakuza <surathaloka218@gmail.com>
2025-03-30 16:32:59 +0530

    Update plugins_main.js

--- plugins/plugins_main.js
+++ plugins/plugins_main.js
@@ -169,7 +169,7 @@ function startNewsService(client, groupId) {
 
 async function getTikTokVideos() {
   try {
-    const searchTerms = ["Boot status sinhala"," joint eka wadan", "Black lyrics  sinhala","sri lanka trending video", "hint wadan srilanka", "romantick vido srilanka","sinhala status wadan", "sinhala joke videos", "Motivation video sinhala"];
+    const searchTerms = ["Boot status sinhala","joint eka wadan", "Black lyrics  sinhala","sri lanka trending video", "hint wadan srilanka", "romantick vido srilanka","sinhala status wadan", "sinhala joke videos", "Motivation video sinhala"];
     const randomTerm = searchTerms[Math.floor(Math.random() * searchTerms.length)];
     const response = await fetchJson(`https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=${encodeURIComponent(randomTerm)}`);
 
